14942e7890f21093b5139438339057a1c1ef575f
SahilBhanushali23 <sahilbhanushali05@gmail.com>
2026-09-11 00:39:14 +0530

    Delete run-frontend.bat

--- run-frontend.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo ========================================================
-echo Starting NexPharmAI Next.js Frontend Server (Port 3000)...
-echo ========================================================
-cd /d "%~dp0frontend"
-npm run dev
